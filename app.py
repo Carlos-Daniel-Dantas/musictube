f4ef5c78437cc7cbfd46d7bf51cd0b32dfaa61fd
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-02-12 17:24:14 -0300

    ADD: Routa e banco de dados criados

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask, render_template
+import mysql.connector
 
 
 app = Flask (__name__)
 
-A
+@app.route("/home", methdots=["GET"])
+@app.route("/")
 
+def pagina_principal():
 
+    #conectando no banco de dados
+    conexao = mysql.connctor.connect(
+        host="127.0.0.1",
+        port="3306",
+        user="root",
+        password="root",
+        database="Musicclube"
+        
+    )
+
+    #Criando o cursor
+    cursor = conexao.cursor(dictionary=True)
+
+    #executando a consulta
+    cursor.execute("SELECT codigo, cantor, duracao, nome, url_imagem, nome_genero, FROM musica;")
+
+    #recuperando os dados
+    musicas = cursor.fetchall()
+
+    #fechando conexão
+    conexao.close()
+
+    return render_template("principal.html", musicas = musicas)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ migrate/estrutura_banco.sql
@@ -0,0 +1,23 @@
+
+create DATABASE IF NOT EXISTS Musicclube;
+
+use Musictube;
+
+CREATE TABLE IF NOT EXISTS genero (
+ nome VARCHAR(30) NOT NULL primary key,
+ icone VARCHAR(100),
+ cor VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS musica (
+ codigo INT NOT NULL primary key auto_increment,
+ cantor VARCHAR(50) NOT NULL,
+ duracao TIME,
+ nome VARCHAR(80),
+ url_imagem CHAR(255),
+ nome_genero VARCHAR(30),
+ constraint fk_musica_genero foreign key (nome_genero) REFERENCES genero (nome)
+);
+
+
+
