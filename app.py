24e5f85e084d3f57b9e238257fc2b4317fae5aef
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-02-20 17:19:03 -0300

    add: Criando uma rota para enviar as músicas pela rota admin, pegando os inputs e criando variaveis para elas serem usadas para inserir.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 from model.genero import recuperar_generos
-from model.musica import recuperar_musicas
+from model.musica import recuperar_musicas, salvar_musica
 
 app = Flask (__name__)
 
@@ -25,9 +25,25 @@ def pagina_principal():
     musicas = recuperar_musicas()
     generos = recuperar_generos()
 
-
-
     return render_template("principal.html", musicas = musicas, generos = generos)
+
+@app.route("/musica/post", methods=["POST"])
+def api_inserir_musica():
+
+    nome_musica = request.form.get("input_nome_musica")
+    cantor_musica = request.form.get("input_cantor_musica")
+    duracao_musica = request.form.get("input_duracao_musica")
+    url_musica = request.form.get("input_url_musica")
+    genero_nome = request.form.get("input_genero_nome")
+
+
+    if salvar_musica(nome_musica, cantor_musica, duracao_musica, url_musica, genero_nome ):
+
+        return redirect("/admin")
+    else:
+        return "Erro ao adicionar música"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
 from model.musica import salvar_musica
 
-salvar_musica("quem não quer sou eu", "Seu Jorge", "5:35","https://i1.sndcdn.com/artworks-M86qR9L0p7dvIDMp-e2BKWA-t500x500.jpg", "Pop" )
+salvar_musica("quem não quer sou eu", "Seu Jorge", "00:05:35","https://i1.sndcdn.com/artworks-M86qR9L0p7dvIDMp-e2BKWA-t500x500.jpg", "Pop" )
 
