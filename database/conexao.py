59fbaf302d6960c0d513e9ef1e1b671444990941
Carlos Daniel <carlos.d.dantas6@aluno.senai.br>
2026-03-27 09:53:00 -0300

    add: Criando conexao nuvem e local

--- database/conexao.py
+++ database/conexao.py
@@ -4,14 +4,26 @@ import mysql.connector
 
 def conectar():
 
-    conexao = mysql.connector.connect(
-        host="127.0.0.1",
-        port="3306",
-        user="root",
-        password="root",
-        database="MusicClube"
-        
-    )
+    tipo_conexao = "NUVEM"
+
+    if tipo_conexao == "LOCAL":
+        conexao = mysql.connector.connect(
+            host="127.0.0.1",
+            port="3306",
+            user="root",
+            password="root",
+            database="MusicClube"
+            
+        )
+    else:
+        conexao = mysql.connector.connect(
+            host="127.0.0.1",
+            port="3306",
+            user="root",
+            password="root",
+            database="MusicClube"
+                
+        )
 
     #Criando o cursor
 
